80f2ff35e113bc1e5596536aa0d2327f66fbbbc1
manuel <bravopaezm.15@gmail.com>
2023-09-06 19:08:35 +0200

    Movido archivo requirements.txt y actualizado settings.py

--- mbdjango422/mbsocialweb/settings.py
+++ mbdjango422/mbsocialweb/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import environ
-import redis
+# import redis
 
 from pathlib import Path
 from firebase_admin import credentials, initialize_app
