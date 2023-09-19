26f2b6ebb6a2354cb38ffabf28095486aeb7b935
manuel <bravopaezm.15@gmail.com>
2023-09-19 04:39:45 +0200

    update security

--- mbdjango422/mbsocialweb/settings.py
+++ mbdjango422/mbsocialweb/settings.py
@@ -42,8 +42,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ALLOWED_HOSTS = ['www.mbsocialweb.net', 'mbweb-production.up.railway.app', '127.0.0.1', 'localhost']
 
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 # Add this line of code to prevent error caused by Django 40 version about trusted origins
-CSRF_TRUSTED_ORIGINS=['https://mbweb-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS=['https://www.mbsocialweb.net']
 
 # Application definition
 
