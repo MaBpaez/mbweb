c502552440df1bd85099ad153930a117a334bd47
manuel <bravopaezm.15@gmail.com>
2023-09-20 03:32:55 +0200

    Actualizado staticfiles

--- mbdjango422/mbsocialweb/settings.py
+++ mbdjango422/mbsocialweb/settings.py
@@ -207,12 +207,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media config
